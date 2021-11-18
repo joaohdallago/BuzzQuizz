7fc7e07e6bbb9d18af201b248992c805b2b744a7
joaohdallago <joaohdallago@gmail.com>
2021-11-18 11:05:25 -0300

    quizz creation structure

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const main = document.querySelector('main');
 
 function openCreateQuizz() {
-    main.innerHTML = 'batata'
+    main.innerHTML = `
+    <div class="quizz-creation">
+        <h2>Comece pelo começo</h2>
+
+        <form>
+            <input type="text" placeholder="Título do seu quizz">
+            <input type="text" placeholder="URL da imagem do seu quizz">
+            <input type="text" placeholder="Quantidade de perguntas do quizz">
+            <input type="text" placeholder="Quantidade de níveis do quizz">
+        </form>
+
+        <button>Prosseguir pra criar perguntas</button>
+    </div>
+    `
 }
