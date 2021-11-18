81c33ee0d19a95d80ea3d081b1e4c7b9b7e5a5e7
joaohdallago <joaohdallago@gmail.com>
2021-11-18 13:03:04 -0300

    quizz cration questions structure

--- script.js
+++ script.js
@@ -1,18 +1,82 @@
 const main = document.querySelector('main');
 
+let newQuizz = {
+	title: "",
+	image: "",
+	questions: [],
+	levels: []
+}
+
 function openCreateQuizz() {
     main.innerHTML = `
     <div class="quizz-creation">
         <h2>Comece pelo começo</h2>
 
-        <form>
-            <input type="text" placeholder="Título do seu quizz">
-            <input type="text" placeholder="URL da imagem do seu quizz">
-            <input type="text" placeholder="Quantidade de perguntas do quizz">
-            <input type="text" placeholder="Quantidade de níveis do quizz">
-        </form>
+        <div>
+            <span id="errorAlert"></span>
+            <form>
+                <input type="text" id="newQuizzTitle" placeholder="Título do seu quizz">
+                <input type="text" id="newQuizzImg" placeholder="URL da imagem do seu quizz">
+                <input type="text" id="newQuizzQuestionsQty" placeholder="Quantidade de perguntas do quizz">
+                <input type="text" id="newQuizzLevelsQty" placeholder="Quantidade de níveis do quizz">
+            </form>
+        </div>
 
-        <button>Prosseguir pra criar perguntas</button>
+        <button onclick="proceedToCreateQuestions()">Prosseguir pra criar perguntas</button>
     </div>
     `
-}
+};
+
+function isValidURL(str) {
+    const regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    return regex.test(str)
+};
+
+function proceedToCreateQuestions() {
+    newQuizz.title = document.querySelector('#newQuizzTitle').value;
+    newQuizz.image = document.querySelector('#newQuizzImg').value;
+
+    newQuizz.questions = [];
+    for (let i=0; i < document.querySelector('#newQuizzQuestionsQty').value; i++) {
+        newQuizz.questions.push({})
+    };
+
+    newQuizz.levels = [];
+    for (let i=0; i < document.querySelector('#newQuizzLevelsQty').value; i++) {
+        newQuizz.levels.push({})
+    };
+
+    const isValid = {
+        title: (newQuizz.title.length >= 20) && (newQuizz.title.length <= 65),
+        img: isValidURL(newQuizz.image),
+        questionsQty: newQuizz.questions.length >= 3,
+        levelsQty: newQuizz.levels.length >= 2
+    };
+
+    if (
+        isValid.title &&
+        isValid.img &&
+        isValid.questionsQty &&
+        isValid.levelsQty
+    ) {
+        main.innerHTML = `
+            <div class="quizz-creation">
+                <h2>Crie suas perguntas</h2>
+
+                <div>
+                    <span id="errorAlert"></span>
+                    <form>
+                        <input type="text" id="newQuizzTitle" placeholder="Título do seu quizz">
+                        <input type="text" id="newQuizzImg" placeholder="URL da imagem do seu quizz">
+                        <input type="text" id="newQuizzQuestionsQty" placeholder="Quantidade de perguntas do quizz">
+                        <input type="text" id="newQuizzLevelsQty" placeholder="Quantidade de níveis do quizz">
+                    </form>
+                </div>
+
+                <button onclick="proceedToCreateQuestions()">Prosseguir pra criar perguntas</button>
+            </div>
+        `;
+    } else {
+        document.querySelector('#errorAlert').innerHTML = 'Preencha os dados corretamente!';
+    };
+};
